1994e370f2c2ff8b0f6c928741246fb0879ef3eb
fernandapieri <fernanda.pieri@smarkio.com.br>
2021-10-08 14:39:50 -0300

    criaçao da classe fornecedor e rota para criar novo fornecedor no banco

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -1,10 +1,21 @@
 const roteador = require('express').Router()
 const TabelaFornecedor = require('./TabelaFornecedor')
+const Fornecedor = require('./Fornecedor')
 
-roteador.use('/', async (req, res) => {
+roteador.get('/', async (req, res) => {
     const resultados = await TabelaFornecedor.listar()
     res.send(
         JSON.stringify(resultados)
+    )
+})
+
+roteador.post('/', async (req,res) => {
+    const dadosRecebidos = req.body
+    const fornecedor = new Fornecedor(dadosRecebidos)
+    
+    await fornecedor.criar()
+    res.send(
+        JSON.stringify(fornecedor)
     )
 })
 
