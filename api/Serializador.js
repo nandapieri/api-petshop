a5e97ef164308b735e411f5cfbdf0a74be1b1e6f
fernandapieri <fernanda.pieri@smarkio.com.br>
2021-10-08 18:40:10 -0300

    adicionado suporte a xml na api

--- api/Serializador.js
+++ api/Serializador.js
@@ -1,13 +1,31 @@
 const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
+const jsontoxml = require('jsontoxml')
 
 class Serializador {
     json(dados) {
         return JSON.stringify(dados)
     }
 
+    xml(dados) {
+        let tag = this.tagSingular
+        if(Array.isArray(dados)) {
+            tag = this.tagPlural
+            dados = dados.map((item) => {
+                return {
+                    [this.tagSingular]:item
+                }
+            })
+        }
+        return jsontoxml({[tag] : dados})
+    }
+
     serializar(dados) {
+        dados = this.filtrar(dados)
         if(this.contentType === 'application/json') {
-            return this.json(this.filtrar(dados))
+            return this.json(dados)
+        } 
+        if(this.contentType === 'application/xml') {
+            return this.xml(dados)
         } 
 
         throw new ValorNaoSuportado(this.contentType)
@@ -41,6 +59,8 @@ class SerializadorFornecedor extends Serializador {
         super()
         this.camposPublicos = ['id', 'empresa','categoria'].concat(camposExtras || [])
         this.contentType = contentType
+        this.tagSingular = 'fornecedor'
+        this.tagPlural = 'fornecedores'
     }
 }
 
@@ -49,6 +69,8 @@ class SerializadorErro extends Serializador {
         super()
         this.camposPublicos = ['id', 'mensagem'].concat(camposExtras || [])
         this.contentType = contentType
+        this.tagSingular = 'erro'
+        this.tagPlural = 'erros'
     }
 }
 
@@ -56,5 +78,5 @@ module.exports = {
     Serializador: Serializador,
     SerializadorFornecedor: SerializadorFornecedor,
     SerializadorErro: SerializadorErro,
-    formatosAceitos: ['application/json']
+    formatosAceitos: ['application/json','application/xml']
 }
